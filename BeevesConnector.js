985c8e1ec899284990b95160accb246a30fb6a17
Altan Orhon <altan@beeves.dev>
2019-05-24 18:57:40 -0700

    Updated

---
+++ BeevesConnector.js
@@ -0,0 +1,75 @@
+class BeevesConnector {
+  constructor(
+    beevesFileContents,
+    beevesActionHandler,
+    shouldLogInvoke = true,
+    shouldLogIncomingMessages = true
+  ) {
+    // handlers should be like:
+    // {'hotword' : hotword_fn, 'stt' : stt_fn, 'nlu'}
+
+    this.beevesFileContents = beevesFileContents;
+    this.beevesActionHandler = beevesActionHandler;
+    this.shouldLogInvoke = shouldLogInvoke;
+    this.shouldLogIncomingMessages = shouldLogIncomingMessages;
+
+    browser.runtime.onInstalled.addListener(this.registerSkillWithBackend);
+    browser.runtime.onInstalled.onMessageExternal(
+      this.handleIncomingMessageFromBeeves
+    );
+  }
+
+  async handleIncomingMessageFromBeeves(message, sender, sendResponse) {
+    if (this.shouldLogIncomingMessages)
+      console.log("BeevesConnector: got message: " + invocationResult);
+
+    if ("type" in message) {
+      try {
+        switch (message["type"]) {
+          case "beevesRPC":
+            let invocationResult = await this.invoke(message);
+            return Promise.resolve(invocationResult);
+            break;
+          default:
+            return Promise.resolve(
+              console.log("BeevesConnector: no understood type in message")
+            );
+        }
+      } catch (err) {
+        console.log("BeevesConnector: messageHandler failed: " + err);
+      }
+    } else
+      return Promise.resolve(
+        console.log(
+          "BeevesConnector: handleIncomingMessageFromBeeves: Message has no type field, skipping"
+        )
+      );
+  }
+
+  async registerSkillWithBackend() {
+    let sending = await browser.runtime.sendMessage(
+      "beeves@beeves.dev",
+      //{'request' : 'registerSkillWithBackend', 'beevesFileContents' : this.beevesFileContents}
+      this.beevesFileContents,
+      {}
+    );
+  }
+
+  async invoke(messageFromBeeves) {
+    if ("functionName" in this.messageFromBeeves) {
+      const functionName = this.messageFromBeeves["functionName"];
+      if (functionName in this.beevesActionHandler) {
+        try {
+          const result = this.beevesActionHandler[functionName].apply(
+            null,
+            message["arguments"]
+          ); // is globalThis neessary?
+          if (this.shouldLogInvoke) console.log("Invoke: " + result);
+          return Promise.resolve(result);
+        } catch (err) {
+          console.log("BeevesConnector: invoke failed: " + err);
+        }
+      }
+    }
+  }
+}
---
+++ background.js
@@ -0,0 +1,52 @@
+//import BeevesConnector from "./BeevesConnector";
+
+async function getJSONData(endpoint) {
+  try {
+    let res = await fetch(endpoint);
+    res = await res.json();
+    //log(res);
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function postData(endpoint, payload) {
+  try {
+    let res = await fetch(endpoint, {
+      method: "post",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    });
+    res = await res.json();
+    //log(res);
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+let beevesActionHandler = {
+  test: args => {
+    console.log(`beevesRPC works! data: ${args}`);
+    return true;
+  },
+  newTab: (room, color) => {
+    const creating = browser.tabs.create({
+      url: "https://example.org"
+    });
+    console.log("creating new tab!");
+  }
+};
+
+async function initializeNewTabExtension() {
+  const beevesFileEndpoint = browser.extension.getURL("beeves.json");
+  let beevesJSON = await getJSONData(beevesFileEndpoint);
+  let beevesConnector = new BeevesConnector(beevesJSON, beevesActionHandler);
+  return Promise.resolve(beevesConnector);
+}
+
+browser.runtime.onInstalled.addListener(initializeNewTabExtension);
